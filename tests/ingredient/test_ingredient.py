a8f36008119e17aceb0ab2a8d104b18b46db3359
eric-nas <ericnascimentocruzc@gmail.com>
2024-02-13 09:47:48 -0300

    Req1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,14 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingrediente = Ingredient('ovo')
+    ingrediente2 = Ingredient('ovo')
+    ingrediente3 = Ingredient('salmão"')
+    assert ingrediente.name == 'ovo'
+    assert ingrediente.__eq__(ingrediente2)
+    assert ingrediente.__hash__() == ingrediente2.__hash__()
+    assert ingrediente.__hash__() != ingrediente3.__hash__()
+    assert ingrediente.__repr__() == "Ingredient('ovo')"
+    assert ingrediente.restrictions == {Restriction.ANIMAL_DERIVED}
